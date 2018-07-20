50d7b9d87819c203ce7e8858814bc1d8432d99da
darothub <darotudeen@gmail.com>
2018-07-20 15:20:26 +0100

    profile page

--- js/profile.js
+++ js/profile.js
@@ -1,5 +1,6 @@
 const display = ()=>{
    var displayStyle = document.getElementById("displayitem")
+   var addBook = document.getElementById("addBook")
    if (displayStyle.style.display == 'none'){
     displayStyle.style.display = 'block'
     addBook.style.display = 'none'
@@ -12,6 +13,7 @@ const display = ()=>{
 
 const displayAdd = ()=>{
     var addBook = document.getElementById("addBook")
+    var displayStyle = document.getElementById("displayitem")
     if (addBook.style.display == 'none'){
         addBook.style.display = 'block'
         displayStyle.style.display = 'none'
