162fcedfa8bfb0d7a335a2bdb0da6db43400fdbc
darothub <darotudeen@gmail.com>
2018-07-20 14:15:19 +0100

    profile page

---
+++ js/profile.js
@@ -0,0 +1,23 @@
+const display = ()=>{
+   var displayStyle = document.getElementById("displayitem")
+   if (displayStyle.style.display == 'none'){
+    displayStyle.style.display = 'block'
+   }
+   else{
+    displayStyle.style.display = 'none'
+   }
+   
+}
+
+const displayAdd = ()=>{
+    var addBook = document.getElementById("addBook")
+    if (addBook.style.display == 'none'){
+        addBook.style.display = 'block'
+    }
+    else{
+        addBook.style.display = 'none'
+    }
+    
+ }
+
+
